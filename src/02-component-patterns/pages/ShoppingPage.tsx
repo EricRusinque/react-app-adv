6a5b99667c2cd05223532a9493e6b6d427dc71ad
unknown <emprenderusinque@hotmail.com>
2022-12-20 14:39:03 -0500

    className en el ProductTitle y ProductImage

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -32,8 +32,8 @@ export const ShoppingPage = () => {
               product={ product }
               className='bg-dark'
             >
-              <ProductImage />
-              <ProductTitle/>
+              <ProductImage className="custom-image" />
+              <ProductTitle className="text-white text-bold"/>
               <ProductButtons/>              
             </ProductCard>
 
